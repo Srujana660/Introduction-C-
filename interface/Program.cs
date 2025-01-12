064add561cfe33ed383e8fd7604cce1f1848bc00
Srujana660 <srujananayanala660@gmail.com>
2025-01-12 13:23:12 +0000

    Add files via upload

---
+++ interface/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+interface Customer1
+{
+    void Print1();
+}
+interface Customer2:Customer1
+{
+    void Print2();
+}
+public class ICustomer: Customer2
+{
+    public void Print1()
+    {
+        Console.WriteLine("Srujana");
+    }
+    public void Print2()
+    {
+        Console.WriteLine("Nayanala");
+    }
+}
+public class program
+{
+    public static void  Main()
+    {
+        Customer1 c1 = new ICustomer();
+        c1.Print1();
+
+    }
+}
