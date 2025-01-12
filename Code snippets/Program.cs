394cd27e6b579c1c19d59704c1cbcf3bf9727fff
Srujana660 <srujananayanala660@gmail.com>
2025-01-12 13:11:39 +0000

    Add files via upload

---
+++ Code snippets/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Runtime.InteropServices;
+namespace Snippets
+{
+    class Program
+    {
+        public static void Main()
+        {
+            checked
+            {
+                for (int k = 0; k < 20; k++)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                } 
+            }
+            for (int j = 0; j < 5; j++)
+            {
+
+            }
+        }
+    }
+}
---
+++ convert to string & to string difference/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace srujana
+{
+    public class Mainclass
+    {
+        public static void Main()
+        {
+            //using ToString
+            //Customer C1 = new Customer();
+            //string str = C1.ToString();
+            //Console.WriteLine(str);
+
+            //using Convert ToString
+            //Customer C1 = new Customer();
+            //string str = Convert.ToString(C1);
+            //Console.WriteLine(str);
+        }
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+    }
+}
