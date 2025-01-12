0fed7a561f3f7d5ee63a5941849c5f4b65b6b698
Srujana <nayanisrujana5@gmail.com>
2025-01-12 12:44:24 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+    {
+        static void Main1()
+        {
+        Console.WriteLine("Hello");
+        }
+    static void Main()
+    {
+        Console.WriteLine("Helloo");
+        Main1();
+        sampleclass
+    }
+
+}
+ /// <summary>
+ /// This is a sample class for creating class
+ /// </summary>
+ public class sampleclass
