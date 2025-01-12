5f6c292f1c77874864ad7a4d3c46dae953c922db
Srujana660 <srujananayanala660@gmail.com>
2025-01-12 14:02:49 +0000

    Add files via upload

---
+++ Read and write/Program.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Enter first name");
+//        string Name = Console.ReadLine();
+//        Console.WriteLine("Nayanala {0}", Name);
+//        //or placeholder syntax to print name
+//        //Console.WriteLine("Dad" + Name);
+
+//    }
+//}   
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first name");
+        string FirstName = Console.ReadLine();
+        Console.WriteLine("Enter Last name");
+        string LastName = Console.ReadLine();
+        Console.WriteLine("Good Morning {0},{1}", FirstName,LastName);
+
+    }
+}
